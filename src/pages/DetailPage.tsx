869553262cf5279ca1f1bb2d250c4c26ccf40f80
as-zini <aszini@naver.com>
2025-12-06 12:09:04 +0900

    feat: 상수 분리 및 detail 페이지 매직넘버 제거

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,19 +1,6 @@
 import { useEffect, useState } from "react";
 import styled from '@emotion/styled';
-import { color } from "../constants";
-import soo7 from '../assets/soo-7.png';
-import soo8 from '../assets/soo-8.png';
-import soo11 from '../assets/soo11.png';
-import soo12 from '../assets/soo12.png';
-import soo13 from '../assets/soo13.png';
-import soo14 from '../assets/soo14.png';
-import soo15 from '../assets/soo15.png';
-import soo16 from '../assets/soo16.png';
-import soo17 from '../assets/soo17.png';
-import soo18 from '../assets/soo18.png';
-import soo19 from '../assets/soo19.png';
-import soo20 from '../assets/soo20.png';
-import soo21 from '../assets/soo21.png';
+import { color, imgMap, pageNum, textMap } from "../constants";
 import { useNavigate, useParams } from "react-router-dom";
 import ProgressBar from "../components/ProgressBar";
 
@@ -21,15 +8,6 @@ const DetailPage = () => {
   const [index, setIndex] = useState(0);
   const navigation = useNavigate();
   const {name} = useParams();
-  const textMap = {
-    'soo': ["예상치 못한 선물은 10배는 더 기쁘다!\n정말 정말 행운이니까!", "여덟번째 글자", " -> 키를 눌러 글자를 완성해보세요!", "수의 글자는 처음부터\n빵!터지는 컨페티 같이 만들고 싶었답니다!", "두 글자를 선택해\n다양하게 그려보고,",
-      "글자의 뼈대가 될 조형을\n선택해,", "전체 글자를 파생시키면\n스케치 완성!", "전체 글자를 파생시키면\n스케치 완성!", "벡터화 해주고,", "전체적인 두께를 맞춰주고,", "전체 배치 및 디테일 수정하고.", "깜찍한 느낌표까지\n찍어주면!",
-      "완성!", ""
-    ]
-  }
-  const imgMap = {
-    'soo': [soo7, soo8, soo11, soo12, soo13, soo14, soo15, soo16, soo17, soo18, soo19, soo20, soo21]
-  }
 
   // 해결 2: 이미지 프리로딩 (배경 깜빡임 방지)
   useEffect(() => {
@@ -54,7 +32,7 @@ const DetailPage = () => {
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'ArrowRight' && index >= 7 && index <= 16) {
+      if (e.key === 'ArrowRight' && index >= 7 && index <= pageNum[name]-1) {
         
         setIndex(prev => prev + 1);
       }
@@ -88,13 +66,13 @@ const DetailPage = () => {
           {textMap[name][index === 1 ? 0 : (index === 3 || index === 4 || index === 5) ? 1 : index >= 7 ? index-5 : textMap[name].length-1]}
         </Text>
       </TextArea>}
-      {index === 17 ? 
+      {index === pageNum[name] ? 
         <HomeButton onClick={() => navigation('/')}>
           <CloverImg/>
           홈으로 !
         </HomeButton>
       :null}
-      {index >= 7 ? <ProgressBar totalSteps={11} currentStep={index-7} color={color[name]}/> : null}
+      {index >= 7 ? <ProgressBar totalSteps={pageNum[name]-6} currentStep={index-7} color={color[name]}/> : null}
     </Container>
   )
 }

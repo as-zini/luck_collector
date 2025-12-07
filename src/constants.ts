4ad31678de885e26d89b809bc3e26c7669c47786
as-zini <aszini@naver.com>
2025-12-07 11:51:14 +0900

    feat: 새, 연, 선 디테일 페이지 추가

--- src/constants.ts
+++ src/constants.ts
@@ -11,26 +11,88 @@ import soo18 from './assets/soo18.png';
 import soo19 from './assets/soo19.png';
 import soo20 from './assets/soo20.png';
 import soo21 from './assets/soo21.png';
+import yeon1 from './assets/yeon1.png';
+import yeon2 from './assets/yeon2.png';
+import yeon3 from './assets/yeon3.png';
+import yeon4 from './assets/yeon4.png';
+import yeon5 from './assets/yeon5.png';
+import yeon6 from './assets/yeon6.png';
+import yeon7 from './assets/yeon7.png';
+import yeon8 from './assets/yeon8.png';
+import yeon9 from './assets/yeon9.png';
+import yeon10 from './assets/yeon10.png';
+import yeon11 from './assets/yeon11.png';
+import yeon12 from './assets/yeon12.png';
+import sae1 from './assets/sae1.png';
+import sae2 from './assets/sae2.png';
+import sae3 from './assets/sae3.png';
+import sae4 from './assets/sae4.png';
+import sae5 from './assets/sae5.png';
+import sae6 from './assets/sae6.png';
+import sae7 from './assets/sae7.png';
+import sae8 from './assets/sae8.png';
+import sae9 from './assets/sae9.png';
+import sae10 from './assets/sae10.png';
+import sae11 from './assets/sae11.png';
+import sae12 from './assets/sae12.png';
+import seon1 from './assets/seon1.png';
+import seon2 from './assets/seon2.png';
+import seon3 from './assets/seon3.png';
+import seon4 from './assets/seon4.png';
+import seon5 from './assets/seon5.png';
+import seon6 from './assets/seon6.png';
+import seon7 from './assets/seon7.png';
+import seon8 from './assets/seon8.png';
+import seon9 from './assets/seon9.png';
+import seon10 from './assets/seon10.png';
+import seon11 from './assets/seon11.png';
+import seon12 from './assets/seon12.png';
+import seon13 from './assets/seon13.png';
+import seon14 from './assets/seon14.png';
+import seon15 from './assets/seon15.png';
+import seon16 from './assets/seon16.png';
+import seon17 from './assets/seon17.png';
 
 export const color = {
   'soo': "#EA504F",
+  'yeon': "#fff",
+  'sae': "#fff",
+  'seon': "#fff",
 }
 
 export const pageNum = {
   'soo':17,
+  'yeon': 16,
+  'sae':16,
+  'seon': 21
 }
 
 export const textMap = {
-  'soo': ["예상치 못한 선물은 10배는 더 기쁘다!\n정말 정말 행운이니까!", "여덟번째 글자", " -> 키를 눌러 글자를 완성해보세요!", "수의 글자는 처음부터\n빵!터지는 컨페티 같이 만들고 싶었답니다!", "두 글자를 선택해\n다양하게 그려보고,",
+  'soo': ["예상치 못한 선물은 10배는 더 기쁘다!\n정말 정말 행운이니까!", "여덟 번째 글자", " → 키를 눌러 글자를 완성해보세요!", "수의 글자는 처음부터\n빵!터지는 컨페티 같이 만들고 싶었답니다!", "두 글자를 선택해\n다양하게 그려보고,",
     "글자의 뼈대가 될 조형을\n선택해,", "전체 글자를 파생시키면\n스케치 완성!", "전체 글자를 파생시키면\n스케치 완성!", "벡터화 해주고,", "전체적인 두께를 맞춰주고,", "전체 배치 및 디테일 수정하고.", "깜찍한 느낌표까지\n찍어주면!",
-    "완성!", ""
+    "", ""
+  ],
+  'yeon': ["우리는 운명인 것이 틀림없어!\n너는 나의 연이니까!", '세 번째 글자', "→ 키를 눌러 글자를 완성해보세요!", "연의 키워드는 ...", "운명의 빨간실 같은\n글자를 만들기로!", "각 글자들이 연결되는 모양이 중요하기 때문에\n먼저 큰 구조를 다양하게 그려보고,",
+    "뼈대에 살을 붙여\n스케치 완성!","벡터화 해주고,","글자 사이 간격 및 기울기를\n다시 조절하고,", "전체 배치 수정 후,", "디테일까지 넣어주면", "", ""
+  ],
+  'sae': ["소울메이트가 있다는 건 ...\n참 ! 럭키 ! 한 일입니다!", "두 번째 글자", "→ 키를 눌러 글자를 완성해보세요!", "롬이의 키워드는 ...", "순정만화 표지 같은\n글자를 만들기로 했습니다", "마음에 드는 조형이 나올때까지\n여러번 펜으로 써 본 후에 ...",
+    "전체 스케치 완성!", "벡터화 해주고,", "굵기 통일하고,", "전체 배치 수정 후에,", "귀여운 하트 느낌표까지\n넣어주면!", "", ""
+  ],
+  'seon': ["언제나 돌아갈 곳이 되어준 엄마, 아빠\n감사하고 사랑합니다!", "열여섯 번째 글자", "→ 키를 눌러 글자를 완성해보세요!", "선과 호의 키워드는 ...", "어떤 글자를 만들어볼까 ...", "그게 있었지 !!\n제일 먼저 떠올랐던 엄마의 쪽지 !",
+    "엄마 글씨체를 닮은 손글씨가 좋겠다!\n다양하게 써보자", "이거 괜찮은데?", "작은 ‘o’의 형태나,", "흘리듯 쓴 ‘ﾩ’의 형태 같은\n엄마의 글씨체를 좀 더 강조해주면 ...",
+    "전체 스케치 완성!", "벡터화 해주고,", "획 수정하고,", "전체적인 배치 수정,", "디테일 수정 후에,", "화룡점정!\n엄마의 햇님도 넣어주면!", "", ""
   ]
 }
 export const imgMap = {
-  'soo': [soo7, soo8, soo11, soo12, soo13, soo14, soo15, soo16, soo17, soo18, soo19, soo20, soo21]
+  'soo': [soo7, soo8, soo11, soo12, soo13, soo14, soo15, soo16, soo17, soo18, soo19, soo20, soo21],
+  'yeon': [yeon1, yeon2, yeon3, yeon4, yeon5, yeon6, yeon7, yeon8, yeon9, yeon10, yeon11, yeon12],
+  'sae': [sae1, sae2, sae3, sae4, sae5, sae6, sae7, sae8, sae9, sae10, sae11, sae12],
+  'seon': [seon1, seon2, seon3, seon4, seon5, seon6, seon7, seon8, seon9, seon10, seon11, seon12, seon13, seon14, seon15, seon16, seon17]
 }
 
 export const mainText = {
   'soo' : "",
-  
+  'yeon': "",
+  'sae': "",
+  'seon': "",
 }
